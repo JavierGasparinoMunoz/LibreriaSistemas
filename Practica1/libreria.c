8d97ca0620790bcb2e623f3e48c479e0a026c5f1
Angelcova <103210569+Angelcova@users.noreply.github.com>
2022-11-10 21:23:12 +0100

    Add files via upload

--- Practica1/libreria.c
+++ Practica1/libreria.c
@@ -6,23 +6,31 @@ int head(int n){
     int count;
     count = 0;
     char buffer[1024];
-
+    //Si el valor de n es mayor que 0 se mostrara las n primeras lineas
     if (n > 0){
         while((count < n) && (fgets(buffer, 1024, stdin) != NULL)){
 		        printf("%s", buffer);
 		        count++;
 	        }
-    
+        //En caso en el que se diga que muestre más lineas de las que en verdad hay se mostraran las lineas y se avisara de que solo se han podido mostrar count lineas
         if (count < n){
             printf("Solo se han podido mostrar %d lineas", count);
         }
+    //Si n = 0, se mostrara un pequeño mensaje avisando de que no hay lineas que mostrar
     } else if (n==0){
         printf("No hay ninguna linea que mostrar");
+    //Si el valor introducido es negativo, saldrá un error ya que n no puede ser negativo
     } else {
-        printf("n no puede ser un numero negativo");
+        errx(1, "n no puede ser un numero negativo");
     }
 }
 
-int fail(int n){
+//Metodo tail
+int tail(int n){
+
+}
+
+//Metodo longlines
+int longlines(int n){
 
 }
--- Practica1/test.c
+++ Practica1/test.c
@@ -1,30 +1,69 @@
 #include <stdio.h>
 #include <string.h>
 #include "libreria.h"
+#include <err.h>
 
+//Metodo encargado de comprobar que el numero y el tipo de los argumentos es valido
 int comprobarArgumentos(int argc, char *argv[]){
+    //Variable que contendra el numero de lineas
     int lineas;
+    //Si se reciben 0 argumentos o más de 2 argumentos se mostrara un mensaje de error
     if ((argc==0) || (argc > 2)) {
-        printf(stderr,"Se han pasado %d argumentos, se tiene que especificar al menos la función",argc);
-    } else {
+        errx(1, "Se han pasado %d argumentos, se tiene que especificar al menos la función",argc); 
+    //Si se reciben 1 o 2 argumentos entrara en el ese     
+    } else { 
+        // El primer argumento debe ser el metodo a elegir, por lo que se comprueba que este no sea un numero y sea head, tail o lonlines.
         if (!isdigit(*argv[0]) && ((strcmp(*argv[0],"head")) || (strcmp(*argv[0],"tail")) || (strcmp(*argv[0],"longlines")))){
+            // En el caso en el que solo se reciba el metodo elegido como argumento, lineas va a valer 10 por defecto
             if (argc==1){
                 lineas=10;
+            // En el caso en el que si se reciba el numero de lineas por argumento, la variable lineas valdra el valor introducido
             } else {
-                lineas=*argv[1];
+                lineas= atoi(*argv[1]);
             }
+        // Si el primer argumento no es el metodo valido que se pueda realizar saltara un mensaje de error
         } else {
-             printf(stderr,"El primer argumento debe ser una de las funciones definidas");
+             errx(1, "El primer argumento debe ser una de las funciones definidas");
         }
     }
+//Se devuelve el valor de la variable lineas
  return lineas;
 }
 
+//Metodo encargado de comprobar que metodo se ha elegido
+int comprobarStrings(char *argv[]){
+    if (strcmp(*argv[0],"head")){
+        return 1;
+    }else if (strcmp(*argv[0],"tail")){
+        return 2;
+    }else if (strcmp(*argv[0],"longlines")){
+        return 3;
+    }else{
+        errx(1, "Error"); 
+    }
+    
+    
+}
+
+//Metodo main del programa
 int main(int argc, char *argv[]){
+    //Variable cuyo valor va a ser el numero de lineas
     int n;
+    //n se inicializa
     n = comprobarArgumentos(argc,argv);
-    if (*argv[0]){
-        
+    
+    switch(comprobarStrings(argv)){
+        case 1:
+            head(n);
+            break;
+        case 2:
+            tail(n);
+            break;
+        case 3:
+            longlines(n);
+            break;
+        default:
+            break;
     }
 }
 
