0f12ed807323587d06a7136c0b4a9e04d264e835
Angelcova <103210569+Angelcova@users.noreply.github.com>
2022-11-13 12:47:13 +0100

    Add files via upload

--- Practica1/libreria.c
+++ Practica1/libreria.c
@@ -49,12 +49,14 @@ int tail(int n)
             } else {
                 indice++;
             }
-        }       
+        }
+    // Si n = 0, no se mostrara ninguna linea, en este caso se muestra un mensaje.       
     } else if (n == 0)
     {
         printf("No hay ninguna linea que mostrar");
-        // Si el valor introducido es negativo, saldrá un error ya que n no puede ser negativo
+        
     }
+    // Si el valor introducido es negativo, saldrá un error ya que n no puede ser negativo
     else
     {
         errx(1, "n no puede ser un numero negativo");
@@ -64,9 +66,84 @@ int tail(int n)
 // Metodo longlines
 int longlines(int n)
 {
+    char *lineasLongLines[] = malloc(n);
+    if (n > 0){
+        int count;
+        count = 0;
+        char buffer[1024];
+        while (fgets(buffer, 1024, stdin) != NULL){
+            if (count < n){
+                *lineasLongLines[count] = *buffer;
+                count++;
+            }else{
+                continuacionLongLines(lineasLongLines,buffer,n);
+            }
+        }
+        Ordenar(lineasLongLines,n);
+        for (size_t i = 0; i < n; i++)
+            {
+                printf("%s",*lineasLongLines[i]);
+            }
+
+    }else if (n == 0)
+    {
+        printf("No hay ninguna linea que mostrar");
+        
+    }
+    // Si el valor introducido es negativo, saldrá un error ya que n no puede ser negativo
+    else
+    {
+        errx(1, "n no puede ser un numero negativo");
+    }
 
 }
 
+//Metodo de sustituir el antiguo menor por el nuevo
+int continuacionLongLines(char *lineasLongLines[], char *buffer, int n){
+    int longitudBuffer, menor;
+    longitudBuffer = strlen(*buffer);
+    menor = buscarNuevoMenor(lineasLongLines,n);
+    if (longitudBuffer > strlen(*linesLongLines[menor])){
+        *lineasLongLines[menor] = *buffer;       
+    }
+    
+    
+}
+
+//Metodo encargado de encontrar la línea que tiene menor longitud
+int buscarNuevoMenor(char *lineasLongLines[], int n){
+    int valor, indice;
+    valor = 0;
+    indice = 0;
+    for (size_t i = 0; i < n; i++)
+    {
+        if (strlen(*lineasLongLines[i])> valor){
+            valor = strlen(*lineasLongLines[i]);
+            indice = i;
+        }
+    }
+    return indice;
+    
+}
+
+//Metodo encargado de ordenar el array
+void Ordenar(char* lineasLongLines[], int n){
+    char * texto;
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; i < n; i++)
+        {
+            if (strlen(*lineasLongLines[j + 1]) > strlen(*lineasLongLines[j])){
+                texto = *lineasLongLines[j];
+                *lineasLongLines[j] = *lineasLongLines[j+1];
+                *lineasLongLines[j+1] = texto;      
+            }
+        }
+        
+    }
+}
+
+//Metodo encargado de meter las ultimas n lineas de la entrada estandar
 int incializarTail(char *lineasTail[],int n)
 {
     int count;
